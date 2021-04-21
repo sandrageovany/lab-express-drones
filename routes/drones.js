5dfa50e62c518507d8e08991ebfb0cf362a94538
Sandra Geovany <sandra.geovany@gmail.com>
2021-04-21 15:37:30 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,37 +1,43 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/Drone.model')
 
 const router = express.Router();
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get('/drones', async (req, res, next) => {
+ let drones= await Drone.find();
+ res.render('drones/list', {drones:await Drone.find()})
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get('/drones/create', async (req, res, next) => {
+  res.render('drones/create-form');
+
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+    
+    const {name, propellers, maxSpeed}= req.body;
+    await Drone.create({name, propellers, maxSpeed});
+    res.redirect('/drones')
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get('/drones/:droneId/edit', async (req, res, next) => {
+const drone= await Drone.findById(req.params.droneId);
+  res.render('drones/update-form', {drone} );
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post('/drones/:droneId/edit', async (req, res, next) => {
+  const {name, propellers, maxSpeed}= req.body;
+  const drone= await Drone.findByIdAndUpdate(req.params.droneId, {
+    name, propellers, maxSpeed })
+    res.redirect('/drones');
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.post('/drones/:droneId/delete', async (req, res, next) => {
+  await Drone.findByIdAndRemove(req.params.droneId);
+  res.redirect('/drones');
+
 });
 
 module.exports = router;
